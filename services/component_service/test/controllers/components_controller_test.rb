1275a23e42dcd700dedfea7dcfbe68229339ab14
Vemrthiss <joeltwh1999@gmail.com>
2022-06-17 20:27:18 +0800

    refactor backend to a single server, also docker and cloud build

--- services/component_service/test/controllers/components_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ComponentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- services/image_service/db/migrate/20220616170957_create_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateImages < ActiveRecord::Migration[7.0]
-  def change
-    create_table :images do |t|
-      t.string :uri
-
-      t.timestamps
-    end
-  end
-end
