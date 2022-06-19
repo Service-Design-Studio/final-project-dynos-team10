315cd6d4ff2f22a6586f6735e473544d94805925
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-06-19 19:35:31 +0800

    model-components.rb

--- server/app/models/component.rb
+++ server/app/models/component.rb
@@ -1,2 +1,44 @@
 class Component < ApplicationRecord
+
+    attr_accessor :status, :component_type, :failing_reasons, :image_url
+
+    def initialize()
+    @status
+    @component_type = ''
+    @failing_reasons = ['']
+    @image_url = ['']
+    end
+
+    def get_status()
+        return @status
+    end
+
+    def set_status(new_status)
+        @status = new_status
+    end
+
+    def get_failing_reasons()
+        return @failing_reasons
+    end
+
+    def set_failing_reasons(new_failing_reasons)
+        @failing_reasons = new_failing_reasons
+    end
+
+    def get_component_type()
+        return @component_type
+    end
+
+    def set_component_type(new_component_type)
+        @component_type = new_component_type
+    end
+
+    def get_images()
+        return @image_url
+    end
+
+    def set_images(new_image_url)
+        @image_url = new_image_url
+    end
+
 end
