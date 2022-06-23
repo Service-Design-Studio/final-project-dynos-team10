98b35fd0c342b26f788d5bdf854a7180a3599acc
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-06-23 13:23:43 +0800

    image model rspec

--- server/spec/models/component_spec.rb
+++ server/spec/models/component_spec.rb
@@ -14,33 +14,22 @@ RSpec.describe Component, :type => :model do
     expect(component).to_not be_valid
   end
 
-  #how to test uniqueness?
-  it "is not valid without a unique component_type" do
-    component = Component.new()
-    expect(component.component_type).to be_unique
-    # OR expect(component).to be_unique
-  end
-
-  # it "is not valid if the component_type is not 0 1 or 2" do
-  #   component = Component.new()
-  #   expect(component.component_type_int).
-
-
   it "is not valid without a workorder_id" do
     component = Component.new(workorder_id: nil)
     expect(component).to_not be_valid
   end
 
-  it "is not valid without a unique workorder_id" do
+    #how to test uniqueness?
+  it "is not valid without a unique workorder_id and unique component_type" do
     component = Component.new()
     expect(component.workorder_id).to be_unique
+    expect(component.component_type).to be_unique
     # OR expect(component).to be_unique
   end
 
   describe "Components" do
-    it { should belong_to(:workorder).without_validating_presence }
+    it { should belong_to(:workorder) }
   end
-
 
 end
 
