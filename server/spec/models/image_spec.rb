55ec865c6ac3867ae0dee53977bf8e8eec53c606
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-07-09 20:44:32 +0800

    added image spec for functions but commented

--- server/spec/models/image_spec.rb
+++ server/spec/models/image_spec.rb
@@ -21,4 +21,24 @@ RSpec.describe Image, type: :model do
   end
 end
 
+# HOW TO PUT IMAGE FILE as attribute
+# describe '.find_one' do
+#   context "given image id" do 
+#     it 'returns the image with that image id' do
+#       image = Image.create_record("1", 1)
+#       expect(Image.find_one(image.id)).to eq(image)
+#     end
+#   end
+# end
+
+# describe '.create_record' do
+#   context "given image file, component_type" do 
+#     it 'creates the image with the given arguements' do
+#       test_image = image.create_record("1",1)
+#       expect(test_image.image_number).to eq("1")
+#       expect(test_image.machine_type).to eq("machine_1")
+#     end
+#   end
+# end
+
 
