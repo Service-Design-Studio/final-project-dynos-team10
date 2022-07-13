1bf33172bcfdcf7b7e3db2eb8b8d6c1a9ee70d67
Joel <joeltwh1999@gmail.com>
2022-07-13 19:41:36 +0800

    try swapping step definition order

--- client/cypress/e2e/steps/steps.js
+++ client/cypress/e2e/steps/steps.js
@@ -172,9 +172,10 @@ Then('I click on the register button, expecting {string}', (expectedOutcome) => 
             statusCode: 200
         })
     })
+    cy.get('.register-btn').click();
     cy.wait('@registration');
     cy.wait(10000);
-    cy.get('.register-btn').click(); // click must be BEHIND intercept
+    // cy.get('.register-btn').click(); // click must be BEHIND intercept
 });
 When('I click on the Log In Now button', () => {
     cy.get('.redirect-login-btn').click();
