6db5e21b03588739073cd2a02f61f1c48d3b0250
kayweeee <90853438+kayweeee@users.noreply.github.com>
2022-07-13 18:03:27 +0800

    own functionaluty and style done

--- client/src/views/QRScanner.jsx
+++ client/src/views/QRScanner.jsx
@@ -1,34 +1,33 @@
-import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
-import {
-    Header,
-    useMantineTheme,
-  } from "@mantine/core";
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { QrReader } from "react-qr-reader";
+import { Header, useMantineTheme, Text } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 
-function QRScanner () {
-    const [WO, setWO] = useState('');
-    const[machine, setMachine] = useState('');
-    const navigate = useNavigate();
-    const theme = useMantineTheme();
+function QRScanner() {
+  const [WO, setWO] = useState("");
+  const [machine, setMachine] = useState("");
+  const [done, setDone] = useState("");
+  const navigate = useNavigate();
+  const theme = useMantineTheme();
 
-    const handleResult = (result, error) => {
-        if (!!result) {
-            const data = (result?.text.split(","))
-            setWO(data[0])
-            setMachine(data[2])
-         
+  const handleResult = (result, error) => {
+    if (!!result) {
+      const data = result?.text.split(",");
+      setWO(data[0]);
+      setMachine(data[2]);
+      
+      navigate('/qc-entry');
     }
 
-          if (!!error) {
-            console.info(error);
-          }
-        }
-    
-      
-    return ( 
+    if (!!error) {
+      console.info(error);
+    }
+  };
+
+  return (
     <div>
-        <Header height={70} p="md">
+
+      <Header height={70} p="md">
         <div
           style={{
             display: "flex",
@@ -41,13 +40,45 @@ function QRScanner () {
         </div>
       </Header>
 
+      <div
+        style={{
+          overflow: "hidden",
+          position: "relative",  
+          height:400,
+          alignItems:"center"
+        }}
+      >
+        <div
+          style={{
+            top: "0px",
+            left: "0px",
+            zIndex: 1,
+            boxSizing: "border-box",
+            border: "50px solid rgba(0, 0, 0, 0.3)",
+            boxShadow: "rgba(255, 0, 0, 0.5) 0px 0px 0px 5px inset",
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+          }}
+        ></div>
+        
         <QrReader
           onResult={handleResult}
-          style={{ height: 240, width: 320 }}
-          scanDelay={3000}
+          scanDelay={2000}
+          style={{
+            display: "block",
+            position: "absolute",
+            overflow: "hidden",
+          }}
+          videoContainerStyle={{marginTop:10}}
         />
-        <p>{WO}, {machine}</p>
-      </div> );
+      </div>
+
+      <Text align="center" style={{ marginTop: 0 }}>
+        Scan QR code
+      </Text>
+    </div>
+  );
 }
 
 export default QRScanner;
