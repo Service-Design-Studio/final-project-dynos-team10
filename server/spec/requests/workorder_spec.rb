dbb47ab749bc734fc521c73dd0bd0d0bf6aab241
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-07-20 00:47:57 +0800

    cleaned up comments

--- server/spec/requests/workorder_spec.rb
+++ server/spec/requests/workorder_spec.rb
@@ -8,11 +8,8 @@ RSpec.describe "Workorders", type: :request do
   describe "PUT #update" do
     it 'returns updated status after updating the component values' do
       workorder = Workorder.create_record("10",1)
-      # work_order_new = Workorder.create_record("15",1)
-
       put workorder_path(workorder), params: { workorder: {id: workorder.id, :machine_type => "2", :workorder_number => "15"}}
       workorder.reload
-      # binding.pry
       expected_json =  {"success"=>true,
                         "result"=>{"workorder_number"=>"15", "machine_type"=>"2", "id"=>workorder.id, "created_at"=>workorder.created_at, "updated_at"=>workorder.updated_at, "user_id"=>nil}}
 
