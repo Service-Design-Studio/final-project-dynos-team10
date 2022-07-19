5c9fc499a5f5aceb2e776365262b08ad8975b2af
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-07-19 19:01:49 +0800

    delete controller test pass

--- server/spec/requests/image_spec.rb
+++ server/spec/requests/image_spec.rb
@@ -10,10 +10,16 @@ RSpec.describe "Images", type: :request do
     it "destroys the requested image" do
       workorder = Workorder.create_record("1",1)
       c1 = Component.create_record(workorder.id,1,false)
-      image = Image.create!(:component_id => c1.id, :public_url => "https://storage.googleapis.com/dynostic-test-bucket/032e11e6-d779-4882-bc01-aee951b65ac3.png", :auth_url => "https://storage.cloud.google.com/dynostic-test-bucket/032e11e6-d779-4882-bc01-aee951b65ac3.png")
+      image = Image.create!(:component_id => c1.id, :public_url => "https://storage.googleapis.com/dynostic-test-bucket/3bd1ac0b-c028-4161-987e-cc16ea5fe2db.png", :auth_url => "https://storage.cloud.google.com/dynostic-test-bucket/3bd1ac0b-c028-4161-987e-cc16ea5fe2db.png")
+      random_filename = "#{SecureRandom.uuid}.png"
+      base64str = "cTNsHa4eJEUrPJnfB79P1LQJ6c2enB4uaXNLMFmfebfILug4jGoDgnAQknM29xYjio6Qsk4/TPuARyJC1wyoVQ=="
+      bucket_image_file = BUCKET.create_image base64str, random_filename
+      image = Image.create_record image.component_id, bucket_image_file
       expect {
-        delete :destroy, { :component_id => c1.id, :id => image.id }
+        delete image_path(image), params: { id: image.id }
       }.to change(Image, :count).by(-1)
+
+
 
     end
 
