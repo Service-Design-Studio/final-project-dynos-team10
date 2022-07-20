bb200d88acf698fd8fee8e0c3202543e62162dd6
Joel <joeltwh1999@gmail.com>
2022-07-20 22:51:10 +0800

    test new workflow with test database in GA container

--- client/src/helpers/axiosHelper.js
+++ client/src/helpers/axiosHelper.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === "development" ? 'https://dynostic-dev-api-oakg5bt7gq-as.a.run.app' : 'https://dynostic-api-oakg5bt7gq-as.a.run.app';
+const baseURL =
+    process.env.NODE_ENV === "test" ? 'http://localhost:5000' :
+    process.env.NODE_ENV === "development" ? 'https://dynostic-dev-api-oakg5bt7gq-as.a.run.app' :
+    'https://dynostic-api-oakg5bt7gq-as.a.run.app';
 
 export const $axios = axios.create({
     baseURL
