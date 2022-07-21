0704e22b8453f231290355d3c831b1cf91dccea6
Joel Tay <joeltwh1999@gmail.com>
2022-07-21 21:55:05 +0800

    fix daemon startup command

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -21,12 +21,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
-
-  allow do
-    origins "*"
-
-    resource "*",
-             headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
 end
