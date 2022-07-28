c33fbcabb2516973a0262bcb5980e55114ba1ba5
t-lihui <tanlihui7@gmail.com>
2022-07-28 14:06:22 +0800

    label result

--- client/src/views/LabelResult.jsx
+++ client/src/views/LabelResult.jsx
@@ -1,7 +1,7 @@
 import { useMemo, useEffect, useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux";
-import { selectWorkorderComponents, updateCurrentComponentName } from "../store/workorder/workorderSlice";
+import { selectWorkorderComponents, updateCurrentComponentName, selectWorkorderNumber, selectCurrentComponent } from "../store/workorder/workorderSlice";
 import { 
     Button,
     Center,
@@ -17,7 +17,10 @@ import {
 import { CheckCircleOutline, HighlightOff } from "@mui/icons-material";
 import ReportFailReasons from "../components/ReportFailReasons";
 import { useListState } from "@mantine/hooks";
-import { $aiAxios } from '../helpers/axiosHelper';
+import { $axios } from '../helpers/axiosHelper';
+import { buildComponentObjWithImages } from "../helpers/componentsHelper";
+import { cloneDeep } from "lodash";
+import { deepCompare } from "../helpers/objectHelper";
 
 /**
  * This page is for ANY labelling result that has been returned from the AI service
@@ -29,6 +32,8 @@ export default function LabelResult() {
     const theme = useMantineTheme();
     const [searchParams, setSearchParams] = useSearchParams();
     const workorderComponents = useSelector(selectWorkorderComponents);
+    const currentWorkorderNumber = useSelector(selectWorkorderNumber);
+    const currentComponent = useSelector(selectCurrentComponent);
     const [hasChosenPhoto, setHasChosenPhoto] = useState(false);
     const chosenLabelPhoto = useMemo(() => {
         const chosenIndex = searchParams.get('chosenLabelPhotoIndex');
@@ -40,8 +45,9 @@ export default function LabelResult() {
         return null;
     }, [searchParams])
 
-    const [gettingResults, setGettingResults] = useState(false);
-    const [noLabel, setNoLabel] = useState(false);
+    const [gettingResults, setGettingResults] = useState(true);
+    const [noLabel, setNoLabel] = useState(true); // if noLabel == true => label status is fail / red 
+    const [submitModal, setSubmitModal] = useState(false);
     const OutcomeIcon = ({height}) => {
         return noLabel ? 
             <HighlightOff style={{fontSize: height, color: theme.colors.red[6]}} /> :
@@ -49,6 +55,7 @@ export default function LabelResult() {
     }
 
     const [reasons, reasonsHandler] = useListState([]);
+    const [ disputeModalOpened, setDisputeModalOpened ] = useState(false);
 
     useEffect(() => {
         (async() => {
@@ -73,13 +80,98 @@ export default function LabelResult() {
         })()
     }, [chosenLabelPhoto])
 
+    const findWorkorderRecord = async() => {
+        try {
+            const { data: response } = await $axios.get(`workorders?workorder_number=${currentWorkorderNumber}`);
+            console.log({response});
+            return response.result.id;
+        } catch(e) {
+            console.error(e);
+            console.log('cannot find current workorder');
+            return null;
+        }
+    }
+
+    const updateComponentRecord = async() => {
+        // get workorder id
+        const workorderId = await findWorkorderRecord();
+        if (!workorderId) {
+            return;
+        }
+        console.log("hi")
+        // here we deal with EDITING/patch requests
+        let response = await $axios.get(`components/${currentComponent.id}`);
+        const dbComponent = response.data.result;
+        response = await $axios.get(`components/${currentComponent.id}/images`);
+        const dbImages = response.data.result;
+        const dbComponentWithImages = buildComponentObjWithImages(dbComponent, dbImages);
+        // Issue 1 (SOLVED): component status only updated after successful upload, so this compare will not detect status changes (yet)
+        // Issue 2 (SOLVED): component status changes to yellow when using camera, if got use camera to add photos, then this will detect status change (to yellow), which is not submittable
+        // to solve these 2 issues, we do a deep clone of the currentComponent object, and set the status to the current state.chosenStatus (from routing)
+        const currentComponentChanges = cloneDeep(currentComponent);
+        console.log(currentComponentChanges.status)
+        if (noLabel) { currentComponentChanges.status = 'red'; }
+        if (!noLabel) { currentComponentChanges.status = 'green'; }
+        console.log(currentComponentChanges.status)
+
+        // Issue 3 (OVERLOOKED): figure out why "differences" is weird when we delete images, check the images array
+        // IS THIS OKAY TO OVERLOOK? BECAUSE our updateImages array does not take the weird missing values from "differences",
+        // but from the sources of truth that is "dbComponentWithImages" and "currentComponentChanges"
+        // we merely use "differences" to detect changes?
+        const differences = deepCompare(dbComponentWithImages, currentComponentChanges);
+        console.log({differences, dbComponentWithImages, currentComponentChanges});
+
+        // if (Object.keys(differences).length === 0) {
+        //     // no differences to be updated found, tell user?
+        //     console.log('no differences/changes to be committed');
+        //     return;
+        // }
+
+        // if (differences.images) {
+        //     // await this?
+        //     updateImages(currentComponentChanges.id, dbComponentWithImages.images, currentComponentChanges.images);
+        // }
+
+        const payload = {
+            component_id: currentComponentChanges.id
+        }
+        if (differences.status) {
+            payload.status = currentComponentChanges.status === 'green';
+        }
+        // if (differences.failingReasons) {
+        //     payload.failing_reasons = payload.status ? [] : currentComponentChanges.failingReasons;
+        // }
+        console.log({payload});
+
+        const finalUpdateResponse = await $axios.patch(`components/${currentComponentChanges.id}`, payload);
+        console.log({finalUpdateResponse});
+
+        submitLabelResult();
+    }
+    
+    const submitLabelResult = () => {
+        setSubmitModal(true);
+        // send to status to database
+        updateComponentRecord();
+
+    }
+
     // ---- dispute modal ----
-    const [disputeModalOpened, setDisputeModalOpened] = useState(false);
+    const [ disputeSent, setDisputeSent ] = useState(false);
     const DisputeModal = () => {
         const proceedWithDispute = () => {
+            setDisputeModalOpened(false);
+            setDisputeSent(true);
+            setNoLabel(false);
             console.log('Disputing...');
+            setGettingResults(true);
+            // call API here
+            setTimeout(() => {
+                setGettingResults(false);
+                // setSubmitModal(true);
+            }, 1500)
         }
-
+        
         return (
             <Modal
                 opened={disputeModalOpened}
@@ -88,22 +180,23 @@ export default function LabelResult() {
                 centered
             >
                 <Text weight={500}>Why are you seeing this?</Text>
-                <List my="md">
-                    <List.Item>
-                        <Text component="span">
-                            Our algorithm has detected that there was
-                            <Text component="span" weight={500}> no label detected </Text>
-                            in the photo you selected.
-                        </Text>
-                    </List.Item>
-                    <List.Item>
-                        <Text component="span">
-                            If you believe that your photo has a label in it and our algorithm was incorrect, please click the "Confirm Dispute" button below, and we will send your image for further analysis.
-                        </Text>
-                    </List.Item>
-                </List>
-                
-                <Button color="red" onClick={proceedWithDispute}>Confirm Dispute</Button>
+                    <List my="md">
+                        <List.Item>
+                            <Text component="span">
+                                Our algorithm has detected that there was
+                                <Text component="span" weight={500}> no label detected </Text>
+                                in the photo you selected.
+                            </Text>
+                        </List.Item>
+                        <List.Item>
+                            <Text component="span">
+                                If you believe that your photo has a label in it and our algorithm was incorrect, please click the "Confirm Dispute" button below, and we will send your image for further analysis.
+                            </Text>
+                        </List.Item>
+                    </List>
+                    
+                    <Button color="red" onClick={proceedWithDispute}>Confirm Dispute</Button>
+
             </Modal>
         )
     }
@@ -169,11 +262,45 @@ export default function LabelResult() {
                                 scrollHeight={150}
                             />
                         </Grid.Col>
-                        <Button mt="md" fullWidth>Submit</Button>
+                        <Button onClick={updateComponentRecord} mt="md" fullWidth>Submit</Button>
                     </Grid>
                 }
+                {
+                    disputeSent && !noLabel && !gettingResults &&
+                    <>
+                    <Center><OutcomeIcon height={100} /></Center>
+                    <Button
+                                fullWidth
+                                mt="sm"
+                                variant="outline"
+                                styles={{
+                                    root: {
+                                        height: 'auto',
+                                        padding: '6px'
+                                    },
+                                    label: {
+                                        whiteSpace: 'normal'
+                                    }
+                                }}
+                                onClick={goBackLabelPhotoReview}
+                            >
+                                Choose Another Photo
+                            </Button>
+                            <Button onClick={updateComponentRecord} mt="md" fullWidth>Submit</Button>
+                    </>
+                    
+                }
+
             </Paper>
+
             <DisputeModal />
+
+            <Modal
+                opened={submitModal}
+                onClose={() => setSubmitModal(false)}
+                title="Upload Successful"
+                centered
+            />
         </>
     )
 }
