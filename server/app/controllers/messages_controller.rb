049c51a373d65c29e061db05776d5399e2e90d3f
Joel Tay <joeltwh1999@gmail.com>
2022-07-30 21:08:11 +0800

    pubsub and etl function

--- server/app/controllers/messages_controller.rb
+++ server/app/controllers/messages_controller.rb
@@ -1,6 +1,9 @@
+require "json"
+
 class MessagesController < ApplicationController
   def create
-    ActionCable.server.broadcast(params[:room], { body: params[:message_body] })
-    render json: 'ok'
+    # ActionCable.server.broadcast(params[:room], { body: params[:message_body] })
+    message = PUBSUB.publish_to_topic("workorders", params[:message_body].to_json)
+    render json: message
   end
 end
