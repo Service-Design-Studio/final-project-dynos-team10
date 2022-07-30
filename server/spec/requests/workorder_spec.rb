af2149f4c542a6b7d0bfae295d9db06cbd338c71
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-07-30 17:11:57 +0800

    tested update controller with new completed field

--- server/spec/requests/workorder_spec.rb
+++ server/spec/requests/workorder_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe "Workorders", type: :request do
 
   describe "PUT #update" do
     it 'returns updated status after updating the component values' do
-      machine_type = MachineType.create_record("m10")
-      workorder = Workorder.create_record("W03",machine_type.id)
-      put workorder_path(workorder), params: { workorder: {id: workorder.id, :machine_type_id => 2, :workorder_number => "15"}}
+      machine_type1 = MachineType.create_record("m10")
+      machine_type2 = MachineType.create_record("m11")
+      workorder = Workorder.create_record("W03",machine_type1.id)
+      put workorder_path(workorder), params: { workorder: {id: workorder.id, :machine_type_id => machine_type2.id, :workorder_number => "W02",:completed =>true}}
       workorder.reload
-      expected_json =  {"success"=>true,
-                        "result"=>{"workorder_number"=>"15", "machine_type_id"=>"2", "id"=>workorder.id, "created_at"=>workorder.created_at, "updated_at"=>workorder.updated_at, "user_id"=>nil}}
-
+      expected_json =  {"result"=>{"completed"=>true, "created_at"=>workorder.created_at, "id"=>workorder.id, "machine_type_id" => machine_type2.id, "updated_at"=> workorder.updated_at , "user_id"=>nil, "workorder_number"=>"W02"}, "success"=>true}
+      puts JSON.parse(response.body)
       expect(JSON.parse(response.body)).to include{expected_json}
     end
   end
