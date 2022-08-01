bdcc4c54e156cd5edbad0dce0e6743e8d7cb5c92
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-08-01 15:27:14 +0800

    removed fail reasons from comp model and updated find fail reasons method

--- server/app/models/component.rb
+++ server/app/models/component.rb
@@ -8,8 +8,8 @@ class Component < ApplicationRecord
     # TODO: validate uniqueness of the above 2 AS a whole unit
     validates :status, inclusion: [true, false] # detection of boolean field presence, different validation because of under the hood ops
 
-    def self.create_record(workorder_id, component_type_id, status, failing_reasons=[""])
-        new_component = Component.create(status: status, workorder_id: workorder_id, failing_reasons: failing_reasons,component_type_id: component_type_id)
+    def self.create_record(workorder_id, component_type_id, status)
+        new_component = Component.create(status: status, workorder_id: workorder_id,component_type_id: component_type_id)
         # return new_component
         # if new_component.nil?
         #
@@ -29,7 +29,7 @@ class Component < ApplicationRecord
     end
 
     def self.get_failing_reason(component_id)
-        Component.find_by(id: component_id).failing_reasons
+        Component.find_by(id: component_id).failing_reasons_types
     end
 
     def self.get_one_images(component_id)
