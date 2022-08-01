7081723234979c07980c83fea4d86eb8e9b09d93
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-08-01 16:21:47 +0800

    create controller fail reasons type

--- server/app/controllers/failing_reasons_type_controller.rb
+++ server/app/controllers/failing_reasons_type_controller.rb
@@ -1,2 +1,17 @@
 class FailingReasonsTypeController < ApplicationController
+  def index
+    all_failing_reasons_types = FailingReasonsType.find_all
+    render json: success_json(all_failing_reasons_types)
+  end
+
+  def create
+    reason = params[:reason]
+    component_type_id = params[:component_type_id]
+    failing_reasons_type = FailingReasonsType.create_record reason, component_type_id
+    if failing_reasons_type.id.nil?
+      render json: fail_json(errors: failing_reasons_type.errors)
+      return
+    end
+    render json: success_json(failing_reasons_type)
+  end
 end
--- server/app/models/failing_reasons_type.rb
+++ server/app/models/failing_reasons_type.rb
@@ -3,9 +3,17 @@ class FailingReasonsType < ApplicationRecord
   has_and_belongs_to_many :components
   validates :reason, presence: true, uniqueness: true
 
+  def self.create_record(reason,component_type_id)
+    FailingReasonsType.create(reason:reason, component_type_id: component_type_id)
+  end
+
   def self.get_components(failing_reasons_type_id)
     @failing_reasons_type = FailingReasonsType.find_by(id: failing_reasons_type_id)
     @failing_reasons_type.components
+  end
+
+  def self.find_all
+    FailingReasonsType.all
   end
 
 end
