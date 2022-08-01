55d4674bc8f4fde1262152bb11432bf4a75bb3d8
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-08-01 17:18:32 +0800

    updated seeds and comp add fail type method

--- server/app/models/component.rb
+++ server/app/models/component.rb
@@ -53,12 +53,9 @@ class Component < ApplicationRecord
     def self.add_failing_reasons_type(component_id,failing_reasons_type_id)
         @component = Component.find_by(id: component_id)
         @failing_reasons_type = FailingReasonsType.find_by(id: failing_reasons_type_id)
-        puts @component.component_type.failing_reasons_types.include?(@failing_reasons_type)
         if @component.component_type.failing_reasons_types.include?(@failing_reasons_type)
             @component.failing_reasons_type.push(@failing_reasons_type) unless  @component.failing_reasons_type.include?(@failing_reasons_type)
             @failing_reasons_type.components.push(@component) unless  @failing_reasons_type.components.include?(@component)
-        else
-        #    what to do?
         end
 
     end
