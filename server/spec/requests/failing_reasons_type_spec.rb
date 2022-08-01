78e09dd863016152225187238ef056f873b92cf1
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-08-01 19:08:04 +0800

    show controller fail reason type

--- server/spec/requests/failing_reasons_type_spec.rb
+++ server/spec/requests/failing_reasons_type_spec.rb
@@ -25,4 +25,21 @@ RSpec.describe "FailingReasonsTypes", type: :request do
     end
   end
 
+  describe "GET /show" do
+    it 'shows the failing reason type from and id' do
+      @machine_type = MachineType.create_record("m9")
+      @component_type = ComponentType.create_record("box")
+      ComponentType.add_machine_type(@machine_type.id,@component_type.id)
+      @work_order = Workorder.create_record("W005",@machine_type.id)
+      @component = Component.create_record(@work_order.id,@component_type.id,false)
+      @failing_reasons_type = FailingReasonsType.create_record("damaged",@component_type.id)
+      Component.update_failing_reasons_types(@component.id,[@failing_reasons_type.id])
+      expected_json =  {"success"=>true, "result"=>{"id"=>@failing_reasons_type.id, "reason"=>"damaged", "created_at"=>@failing_reasons_type.created_at, "updated_at"=>@failing_reasons_type.updated_at, "component_type_id"=>@component_type.id}}
+      expected_json = JSON.parse(expected_json.to_json)
+      get failing_reasons_type_path(@failing_reasons_type)
+      expect(JSON.parse(response.body)).to eq(expected_json)
+    end
+  end
+
+
 end
