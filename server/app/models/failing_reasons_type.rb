c491e5d824c91c75ffab36403ae89cd93fd49266
Umang <umang_gupta@mymail.sutd.edu.sg>
2022-08-01 16:11:28 +0800

    add get comp method fail reasons type model

--- server/app/models/failing_reasons_type.rb
+++ server/app/models/failing_reasons_type.rb
@@ -2,4 +2,10 @@ class FailingReasonsType < ApplicationRecord
   belongs_to :component_type
   has_and_belongs_to_many :components
   validates :reason, presence: true, uniqueness: true
+
+  def self.get_components(failing_reasons_type_id)
+    @failing_reasons_type = FailingReasonsType.find_by(id: failing_reasons_type_id)
+    @failing_reasons_type.components
+  end
+
 end
